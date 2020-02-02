23dbaf357e69496c06f3c5f13410512ef06e745a
Rick Beerendonk <rick@beerendonk.com>
2020-02-02 20:19:44 +0100

    Add "List Rendering - Else-If" example.

---
+++ 04 - List Rendering - TODO/c. Else-If/src/App.svelte
@@ -0,0 +1,20 @@
+<script>
+  let language = 'nl';
+</script>
+
+{#if language === 'en'}
+  <span>English</span>
+{:else if language === 'es'}
+  <span>Español</span>
+{:else if language === 'nl'}
+  <span>Nederlands</span>
+{:else}
+  <span>Unknown</span>
+{/if}
+
+<!--
+  Block:
+  # = Open
+  : = Continue
+  / = Close
+-->
