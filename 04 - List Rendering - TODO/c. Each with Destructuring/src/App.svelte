ea70452a26c4296d9660fe4a565b24b0702b5ec1
Rick Beerendonk <rick@beerendonk.com>
2020-02-02 21:02:05 +0100

    Add "List Rendering - Each with Destructuring" example.

---
+++ 04 - List Rendering - TODO/c. Each with Destructuring/src/App.svelte
@@ -0,0 +1,13 @@
+<script>
+  const persons = [
+    { name: 'Alexandra', age: 35 },
+    { name: 'Benjamin', age: 21 },
+    { name: 'Charlotte', age: 57 }
+  ];
+</script>
+
+<ul>
+  {#each persons as { name, age }}
+    <li>{name} is {age} years old</li>
+  {/each}
+</ul>
