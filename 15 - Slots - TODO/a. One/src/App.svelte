64b4f0ec2d8fdafa5d162740282883d98cd4a7da
Rick Beerendonk <rick@beerendonk.com>
2020-02-03 22:17:26 +0100

    Add "Slots - One" example.

---
+++ 15 - Slots - TODO/a. One/src/App.svelte
@@ -0,0 +1,10 @@
+<script>
+  import Container from './Container';
+</script>
+
+<Container>
+  <em>First</em>
+</Container>
+
+<!-- European Union Public License version 1.2 -->
+<!-- Copyright © 2020 Rick Beerendonk -->
