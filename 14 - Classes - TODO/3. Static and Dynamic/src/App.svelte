5ca20fd99a01505f85ed526f3a3df2394bfc2ce8
Rick Beerendonk <rick@beerendonk.com>
2020-02-05 22:29:21 +0100

    Add "Classes - Static and Dynamic" example.

---
+++ 14 - Classes - TODO/3. Static and Dynamic/src/App.svelte
@@ -0,0 +1,22 @@
+<script>
+  let isDynamic = false;
+</script>
+
+<style>
+  .static {
+    color: red;
+  }
+
+  .dynamic {
+    text-decoration: underline;
+  }
+</style>
+
+<h1
+  class={`static ${isDynamic ? 'dynamic' : ''}`}
+  on:click={() => (isDynamic = !isDynamic)}>
+  Hello Svelte!
+</h1>
+
+<!-- European Union Public License version 1.2 -->
+<!-- Copyright © 2020 Rick Beerendonk -->
