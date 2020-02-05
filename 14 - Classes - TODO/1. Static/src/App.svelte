9fa1f0c4ed3a42866e6ce83a36f050195bcc3294
Rick Beerendonk <rick@beerendonk.com>
2020-02-05 22:18:14 +0100

    Add "Classes - Static" example.

---
+++ 14 - Classes - TODO/1. Static/src/App.svelte
@@ -0,0 +1,10 @@
+<style>
+  .static {
+    color: red;
+  }
+</style>
+
+<h1 class="static">Hello Svelte!</h1>
+
+<!-- European Union Public License version 1.2 -->
+<!-- Copyright © 2020 Rick Beerendonk -->
