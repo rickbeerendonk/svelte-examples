d9d1a1b65064bc2d8883d70b82d79386b2845329
Rick Beerendonk <rick@beerendonk.com>
2020-02-05 22:30:36 +0100

    Add "Classes - Dynamic" example.

---
+++ 14 - Classes - TODO/2. Dynamic/src/App.svelte
@@ -0,0 +1,18 @@
+<script>
+  let isDynamic = false;
+</script>
+
+<style>
+  .dynamic {
+    text-decoration: underline;
+  }
+</style>
+
+<h1
+  class={isDynamic ? 'dynamic' : ''}
+  on:click={() => (isDynamic = !isDynamic)}>
+  Hello Svelte!
+</h1>
+
+<!-- European Union Public License version 1.2 -->
+<!-- Copyright © 2020 Rick Beerendonk -->
