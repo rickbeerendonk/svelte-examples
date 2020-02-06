ba126bf0c14ffc29597e9bc2d215ebc9dee31ae8
Rick Beerendonk <rick@beerendonk.com>
2020-02-06 08:48:28 +0100

    Add "Slots - Default Value" example.

---
+++ 15 - Slots - TODO/b. Default Value/src/App.svelte
@@ -0,0 +1,9 @@
+<script>
+  import Container from './Container';
+</script>
+
+<!-- No slot value (not even a comment!) -->
+<Container />
+
+<!-- European Union Public License version 1.2 -->
+<!-- Copyright © 2020 Rick Beerendonk -->
