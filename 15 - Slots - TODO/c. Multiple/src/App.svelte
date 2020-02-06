bdc09215e84da1fbce7ca3b722b18c9473507690
Rick Beerendonk <rick@beerendonk.com>
2020-02-06 09:18:25 +0100

    Add "Slots - Multiple" example.

---
+++ 15 - Slots - TODO/c. Multiple/src/App.svelte
@@ -0,0 +1,19 @@
+<script>
+  import Container from './Container';
+</script>
+
+<Container>
+  <!-- v-slot:default -->
+  <u>Default</u>
+
+  <span slot="first">
+    <em>First</em>
+  </span>
+
+  <span slot="second">
+    <b>Second</b>
+  </span>
+</Container>
+
+<!-- European Union Public License version 1.2 -->
+<!-- Copyright © 2020 Rick Beerendonk -->
