2b0478ae3a85322e7239ce14258ffe2907038f64
Rick Beerendonk <rick@beerendonk.com>
2020-02-06 08:51:20 +0100

    Add "Slots - Default Value - Issue with Comment" example.

---
+++ 15 - Slots - TODO/c. Default Value - i. Issue with Comment/src/App.svelte
@@ -0,0 +1,10 @@
+<script>
+  import Container from './Container';
+</script>
+
+<Container>
+  <!-- Comment only slot value prevents default value from being used -->
+</Container>
+
+<!-- European Union Public License version 1.2 -->
+<!-- Copyright © 2020 Rick Beerendonk -->
