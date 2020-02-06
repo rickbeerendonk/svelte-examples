80a26250d0efb687c0172383456639d59bbf018b
Rick Beerendonk <rick@beerendonk.com>
2020-02-06 08:37:29 +0100

    Add "Classes - Using Directive - Shortcut" example.

---
+++ 14 - Classes/3. Using Directive - i. Shortcut/src/App.svelte
@@ -0,0 +1,20 @@
+<script>
+  let dynamic = false;
+</script>
+
+<style>
+  .static {
+    color: red;
+  }
+
+  .dynamic {
+    text-decoration: underline;
+  }
+</style>
+
+<h1 class="static" class:dynamic on:click={() => (dynamic = !dynamic)}>
+  Hello Svelte!
+</h1>
+
+<!-- European Union Public License version 1.2 -->
+<!-- Copyright © 2020 Rick Beerendonk -->
