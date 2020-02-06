d818ca9fef98a2bd4cb0c14b59f34d4f6bba58d1
Rick Beerendonk <rick@beerendonk.com>
2020-02-06 22:32:28 +0100

    Add "Slots - Slot Props - Destructuring" example.

---
+++ 15 - Slots/d. Slot Props - ii. Destructuring/src/App.svelte
@@ -0,0 +1,11 @@
+<script>
+  import Container from './Container';
+</script>
+
+<!-- Destructure data -->
+<Container let:data={{ source }}>
+  <em>First: {source}</em>
+</Container>
+
+<!-- European Union Public License version 1.2 -->
+<!-- Copyright © 2020 Rick Beerendonk -->
---
+++ 15 - Slots/d. Slot Props - ii. Destructuring/src/Container.svelte
@@ -0,0 +1,27 @@
+<script>
+  let data = {
+    source: 'Container'
+  };
+</script>
+
+<style>
+  .container {
+    background-color: lightgray;
+    padding: 10px;
+  }
+
+  .content {
+    background-color: white;
+    padding: 10px;
+  }
+</style>
+
+<div class="container" let:data>
+  Normal:
+  <div class="content">
+    <slot {data} />
+  </div>
+</div>
+
+<!-- European Union Public License version 1.2 -->
+<!-- Copyright © 2020 Rick Beerendonk -->
