fafa12b6ac07ec966b83b2b5d6014a4383248751
Rick Beerendonk <rick@beerendonk.com>
2020-02-09 22:12:47 +0100

    Add "Event Modifiers - preventDefault" examples.

---
+++ 08 - Event Modifiers - TODO/1. preventDefault/b. With JS/src/App.svelte
@@ -0,0 +1,15 @@
+<script>
+  let date = new Date();
+
+  function handleSubmit(e) {
+    e.preventDefault();
+  }
+</script>
+
+<form on:submit={handleSubmit}>
+  <p>Page loaded at: {date.toLocaleTimeString()}</p>
+  <button>Submit</button>
+</form>
+
+<!-- European Union Public License version 1.2 -->
+<!-- Copyright © 2020 Rick Beerendonk -->
