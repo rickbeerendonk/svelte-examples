aa6d56566303bda762d40ca7d7a3e95436284475
Rick Beerendonk <rick@beerendonk.com>
2020-02-09 09:35:22 +0100

    Demonstrate use of different selectors.

--- 02 - Data Binding - TODO/3. Style/src/App.svelte
+++ 02 - Data Binding - TODO/3. Style/src/App.svelte
@@ -2,11 +2,14 @@
   h1 {
     font-size: 48px;
     font-style: italic;
+  }
+
+  .important {
     text-decoration: underline;
   }
 </style>
 
-<h1>Hello Svelte!</h1>
+<h1 class="important">Hello Svelte!</h1>
 
 <!-- European Union Public License version 1.2 -->
 <!-- Copyright © 2020 Rick Beerendonk -->
