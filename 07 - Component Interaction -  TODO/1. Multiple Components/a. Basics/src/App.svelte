b92857983db8cf5f1ecec2daa03676df142486ff
Rick Beerendonk <rick@beerendonk.com>
2020-02-09 18:04:16 +0100

    Add "Component Interaction - Multiple Components - Basics" example.

---
+++ 07 - Component Interaction -  TODO/1. Multiple Components/a. Basics/src/App.svelte
@@ -0,0 +1,11 @@
+<script>
+  import ComponentA from './ComponentA';
+  import ComponentB from './ComponentB';
+</script>
+
+<h1>App</h1>
+<ComponentA />
+<ComponentB />
+
+<!-- European Union Public License version 1.2 -->
+<!-- Copyright © 2020 Rick Beerendonk -->
---
+++ 07 - Component Interaction -  TODO/1. Multiple Components/a. Basics/src/ComponentB.svelte
@@ -0,0 +1,11 @@
+<style>
+  h2 {
+    background-color: #444;
+    color: white;
+  }
+</style>
+
+<h2>Component B</h2>
+
+<!-- European Union Public License version 1.2 -->
+<!-- Copyright © 2020 Rick Beerendonk -->
