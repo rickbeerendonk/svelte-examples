435d0e9e5d60571fa380008c857281aeefa7c8a1
Rick Beerendonk <rick@beerendonk.com>
2020-02-09 10:14:37 +0100

    Add "Bindings - TextArea" example

---
+++ 06 - Bindings - TODO/3. TextArea/src/App.svelte
@@ -0,0 +1,36 @@
+<script>
+  let text = `HTML doesn't support multiple lines in attributes.
+Therefor place content between opening and closing tag.
+In JS you can add multiline strings in attributes.
+So in Svelte use bind:value={} instead.`;
+</script>
+
+<style>
+  textarea {
+    height: 4rem;
+    width: 25rem;
+  }
+</style>
+
+<label>
+  Without binding:
+  <br />
+  <!-- prettier-ignore-start -->
+  <textarea>
+    HTML doesn't support multiple lines in attributes. Therefor place content
+    between opening and closing tag. In JS you can add multiline strings in
+    attributes. So in Svelte use bind:value={'{'}} instead.
+  </textarea>
+  <!-- prettier-ignore-end -->
+</label>
+
+<br />
+
+<label>
+  With binding:
+  <br />
+  <textarea bind:value={text} />
+</label>
+
+<!-- European Union Public License version 1.2 -->
+<!-- Copyright © 2020 Rick Beerendonk -->
