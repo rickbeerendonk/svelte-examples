9a6e0c88fa280d9d9c5543487fa1f7e77d486964
Rick Beerendonk <rick@beerendonk.com>
2020-02-09 09:40:50 +0100

    Add "Bindings - Input Group - Checkbox" example.

---
+++ 06 - Bindings - TODO/2. Input Group - b. Checkbox/src/App.svelte
@@ -0,0 +1,20 @@
+<script>
+  let checked = ['B'];
+</script>
+
+<label>
+  <input type="checkbox" bind:group={checked} value="A" />
+  A
+</label>
+<label>
+  <input type="checkbox" bind:group={checked} value="B" />
+  B
+</label>
+<label>
+  <input type="checkbox" bind:group={checked} value="C" />
+  C
+</label>
+<h1>{JSON.stringify(checked)}</h1>
+
+<!-- European Union Public License version 1.2 -->
+<!-- Copyright © 2020 Rick Beerendonk -->
