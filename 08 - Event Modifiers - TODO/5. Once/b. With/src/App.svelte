7af3f3349fd89884fa90780705e112aad9973fec
Rick Beerendonk <rick@beerendonk.com>
2020-02-09 23:07:15 +0100

    Add "Event Modifiers - Once" examples.

---
+++ 08 - Event Modifiers - TODO/5. Once/b. With/src/App.svelte
@@ -0,0 +1,8 @@
+<script>
+  let count = 0; // Will NOT become more than 1
+</script>
+
+<h1 on:click|once={() => count++}>This has been clicked {count} times!</h1>
+
+<!-- European Union Public License version 1.2 -->
+<!-- Copyright © 2020 Rick Beerendonk -->
