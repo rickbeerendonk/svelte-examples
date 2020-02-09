afc6ae9a0d53bf953b1287a549c64bc8e9289ebe
Rick Beerendonk <rick@beerendonk.com>
2020-02-09 10:24:10 +0100

    Add remark.

--- 02 - Data Binding - TODO/2. View to Data/a. Event changing Data/src/App.svelte
+++ 02 - Data Binding - TODO/2. View to Data/a. Event changing Data/src/App.svelte
@@ -2,6 +2,7 @@
   let count = 0;
 </script>
 
+<!-- Some frameworks have performance problems with inline handlers, NOT Svelte -->
 <h1 on:click={() => count++}>This has been clicked {count} times!</h1>
 
 <!-- European Union Public License version 1.2 -->
