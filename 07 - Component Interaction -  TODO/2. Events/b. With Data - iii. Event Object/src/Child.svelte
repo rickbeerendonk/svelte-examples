5d273c18bd170e8a34dbe5f692c3293fbcdbafae
Rick Beerendonk <rick@beerendonk.com>
2020-02-09 11:41:16 +0100

    Add "Component Interaction - Events - With Data - Event Object" example.

---
+++ 07 - Component Interaction -  TODO/2. Events/b. With Data - iii. Event Object/src/Child.svelte
@@ -0,0 +1,18 @@
+<script>
+  import { createEventDispatcher } from 'svelte';
+
+  const dispatch = createEventDispatcher();
+
+  let name = 'Svelte';
+
+  function changed(e) {
+    dispatch('change', {
+      name: e.target.value
+    });
+  }
+</script>
+
+<input value={name} on:keyup={changed} />
+
+<!-- European Union Public License version 1.2 -->
+<!-- Copyright © 2020 Rick Beerendonk -->
