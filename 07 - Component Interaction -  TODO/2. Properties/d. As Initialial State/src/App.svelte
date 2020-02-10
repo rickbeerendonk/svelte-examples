194be7805c23ae781003e89d6a81faf1d8df7da9
Rick Beerendonk <rick@beerendonk.com>
2020-02-10 07:26:10 +0100

    Add "Component Interaction - Properties - As Initial State" example.

---
+++ 07 - Component Interaction -  TODO/2. Properties/d. As Initialial State/src/App.svelte
@@ -0,0 +1,14 @@
+<script>
+  export let count;
+
+  let items = [...Array(count).keys()];
+</script>
+
+<ul>
+  {#each items as item}
+    <li>{item}</li>
+  {/each}
+</ul>
+
+<!-- European Union Public License version 1.2 -->
+<!-- Copyright © 2020 Rick Beerendonk -->
