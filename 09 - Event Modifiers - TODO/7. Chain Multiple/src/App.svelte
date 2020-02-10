96445b93ee6d98594b3917cd799ca91a43f3dfeb
Rick Beerendonk <rick@beerendonk.com>
2020-02-10 21:35:26 +0100

    Add "Event Modifiers - Chain Multiple" example.

---
+++ 09 - Event Modifiers - TODO/7. Chain Multiple/src/App.svelte
@@ -0,0 +1,43 @@
+<script>
+  let result = [];
+
+  function parentCapturingClicked(event) {
+    result = [
+      ...result,
+      `${event.currentTarget.id} received event for ${event.target.id} in capturing fase.`
+    ];
+  }
+
+  function childClicked(event) {
+    result = [...result, `${event.target.id} received event.`];
+  }
+</script>
+
+<style>
+  .comment {
+    color: gray;
+  }
+
+  .result {
+    color: lightgray;
+  }
+</style>
+
+<p class="comment">
+  Observe that when clicking the child repeatedly, the parent's on:click|capture
+  handler is triggered ONCE BEFORE the child.
+</p>
+
+<div id="parent" on:click|capture|once={parentCapturingClicked}>
+  Parent
+  <div id="child" on:click={childClicked}>Child</div>
+</div>
+
+<ul class="result">
+  {#each result as item}
+    <li>{item}</li>
+  {/each}
+</ul>
+
+<!-- European Union Public License version 1.2 -->
+<!-- Copyright © 2020 Rick Beerendonk -->
