0cd2eac24439976e0a95b525c45a07e30eeedd3f
Rick Beerendonk <rick@beerendonk.com>
2020-02-10 09:58:59 +0100

    Add Debugging example.

---
+++ Debugging/1. Webpack/src/components/EditBox.svelte
@@ -0,0 +1,14 @@
+<script>
+  import { createEventDispatcher } from 'svelte';
+
+  const dispatch = createEventDispatcher();
+
+  export let value;
+
+  function changed(e) {
+    dispatch('change', e.target.value);
+  }
+</script>
+
+<!-- warn-disable a11y-autofocus -->
+<input autofocus {value} on:keyup={changed} />
---
+++ Debugging/1. Webpack/src/components/Greeting.svelte
@@ -0,0 +1,12 @@
+<script>
+  export let name;
+</script>
+
+<style>
+  h1 {
+    color: #d676ff;
+  }
+</style>
+
+<svelte:options immutable={true} />
+<h1>Hello {name}!</h1>
