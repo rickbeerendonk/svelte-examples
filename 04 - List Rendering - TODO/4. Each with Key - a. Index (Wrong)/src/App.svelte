6e615b4625c9c8d857c844c2b156b9f13b447ece
Rick Beerendonk <rick@beerendonk.com>
2020-02-10 13:50:26 +0100

    Add "List Rendering - Each with Key - Index (Wrong)" example.

---
+++ 04 - List Rendering - TODO/4. Each with Key - a. Index (Wrong)/src/App.svelte
@@ -0,0 +1,25 @@
+<script>
+  let items = ['one', 'two', 'three'];
+  let itemId = 4;
+
+  function insertTop() {
+    items = [itemId++, ...items];
+  }
+</script>
+
+<p style="color: gray">
+  Add some text in an input and observe what happens if you click the button.
+</p>
+
+<button on:click={insertTop}>Insert at Top</button>
+
+<ol>
+  {#each [...items] as item, index (index)}
+    <li value={index}>
+      <input placeholder={item} />
+    </li>
+  {/each}
+</ol>
+
+<!-- European Union Public License version 1.2 -->
+<!-- Copyright © 2020 Rick Beerendonk -->
