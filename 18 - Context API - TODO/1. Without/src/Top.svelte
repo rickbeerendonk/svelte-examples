831dd127ce164ce2135de4c14b8219e65ff5ef4d
Rick Beerendonk <rick@beerendonk.com>
2020-02-11 19:53:26 +0100

    Add "Context API - Without" example.

---
+++ 18 - Context API - TODO/1. Without/src/Top.svelte
@@ -0,0 +1,10 @@
+<script>
+  import Middle from './Middle';
+
+  export let color;
+</script>
+
+<Middle {color} />
+
+<!-- European Union Public License version 1.2 -->
+<!-- Copyright © 2020 Rick Beerendonk -->
