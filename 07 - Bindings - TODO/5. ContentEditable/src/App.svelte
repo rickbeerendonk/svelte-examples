63fcf4d1dca3df55f1d593cec228adb26ed87cb9
Rick Beerendonk <rick@beerendonk.com>
2020-02-11 23:15:18 +0100

    Add "Bindings - Input - ContentEditable" example.

---
+++ 07 - Bindings - TODO/5. ContentEditable/src/App.svelte
@@ -0,0 +1,15 @@
+<script>
+  let text = 'Svelte';
+</script>
+
+<style>
+  div {
+    border: 1px solid gray;
+  }
+</style>
+
+<div contenteditable="true" bind:innerHTML={text} />
+<p>{text}</p>
+
+<!-- European Union Public License version 1.2 -->
+<!-- Copyright © 2020 Rick Beerendonk -->
