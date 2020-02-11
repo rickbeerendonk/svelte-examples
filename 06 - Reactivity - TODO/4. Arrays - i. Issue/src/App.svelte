2d2bc99d82d0af49bbe1c089ab37b7297bb37960
Rick Beerendonk <rick@beerendonk.com>
2020-02-11 23:57:45 +0100

    Add "Reactivity - Arrays - Issue" example.

---
+++ 06 - Reactivity - TODO/4. Arrays - i. Issue/src/App.svelte
@@ -0,0 +1,24 @@
+<script>
+  let arr = [];
+
+  function handleClick() {
+    arr.push(new Date());
+    console.log(arr.length);
+  }
+</script>
+
+<style>
+  .comment {
+    color: gray;
+  }
+</style>
+
+<p class="comment">
+  Observe that clicking doesn't update the number (reflection of an internal
+  array length). Open the console to see the real array length (proof of it's
+  update).
+</p>
+<h1 on:click={handleClick}>This has been clicked {arr.length} items!</h1>
+
+<!-- European Union Public License version 1.2 -->
+<!-- Copyright © 2020 Rick Beerendonk -->
