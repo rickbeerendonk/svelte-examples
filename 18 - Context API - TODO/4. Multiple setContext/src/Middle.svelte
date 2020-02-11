1d71f2fdde143e4ca0e5cc4c05a61e1c455857b6
Rick Beerendonk <rick@beerendonk.com>
2020-02-11 22:12:12 +0100

    Add "Context API - Multiple setContext" example.

---
+++ 18 - Context API - TODO/4. Multiple setContext/src/Middle.svelte
@@ -0,0 +1,12 @@
+<script>
+  import { setContext } from 'svelte';
+  import colorContext from './color-context';
+  import Bottom from './Bottom';
+
+  setContext(colorContext, { color: 'green' });
+</script>
+
+<Bottom />
+
+<!-- European Union Public License version 1.2 -->
+<!-- Copyright © 2020 Rick Beerendonk -->
---
+++ 18 - Context API - TODO/4. Multiple setContext/src/Top.svelte
@@ -0,0 +1,12 @@
+<script>
+  import { setContext } from 'svelte';
+  import colorContext from './color-context';
+  import Middle from './Middle';
+
+  setContext(colorContext, { color: 'red' });
+</script>
+
+<Middle />
+
+<!-- European Union Public License version 1.2 -->
+<!-- Copyright © 2020 Rick Beerendonk -->
