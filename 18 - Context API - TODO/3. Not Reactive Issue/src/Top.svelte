32485307b60a483adc4dc6a5922f6cd60bb13a15
Rick Beerendonk <rick@beerendonk.com>
2020-02-11 22:09:41 +0100

    Add "Context API - Not Reactive Issue" example.

---
+++ 18 - Context API - TODO/3. Not Reactive Issue/src/Top.svelte
@@ -0,0 +1,28 @@
+<script>
+  import { setContext } from 'svelte';
+  import colorContext from './color-context';
+  import Middle from './Middle';
+
+  let color = 'red';
+
+  function handleSwitch() {
+    color = color === 'red' ? 'green' : 'red';
+  }
+
+  setContext(colorContext, { color });
+</script>
+
+<style>
+  .comment {
+    color: gray;
+  }
+</style>
+
+<Middle />
+<button on:click={handleSwitch}>Switch color</button>
+<p class="comment">
+  Observe that the button doesn't work, because a context is not reactive.
+</p>
+
+<!-- European Union Public License version 1.2 -->
+<!-- Copyright © 2020 Rick Beerendonk -->
