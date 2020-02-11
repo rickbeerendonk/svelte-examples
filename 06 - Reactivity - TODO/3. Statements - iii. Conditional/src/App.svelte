dcb9c784a322ed4607b7a16f972114a994de2e0b
Rick Beerendonk <rick@beerendonk.com>
2020-02-11 23:36:43 +0100

    Add "Reactivity - Statements - Conditional" example.

---
+++ 06 - Reactivity - TODO/3. Statements - iii. Conditional/src/App.svelte
@@ -0,0 +1,16 @@
+<script>
+  let count = 0;
+  $: if (count > 0 && !(count % 2)) {
+    // Alert every even number, except for 0
+    alert(`Count became ${count}`);
+  }
+
+  function handleClick() {
+    count += 1;
+  }
+</script>
+
+<h1 on:click={handleClick}>This has been clicked {count} times!</h1>
+
+<!-- European Union Public License version 1.2 -->
+<!-- Copyright © 2020 Rick Beerendonk -->
