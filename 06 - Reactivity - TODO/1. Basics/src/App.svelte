4d412876a106154541701225360fcc2b0fc08a09
Rick Beerendonk <rick@beerendonk.com>
2020-02-11 23:19:35 +0100

    Add "Reactivity - Basics" example.

---
+++ 06 - Reactivity - TODO/1. Basics/src/App.svelte
@@ -0,0 +1,12 @@
+<script>
+  let count = 0;
+
+  function handleClick() {
+    count += 1;
+  }
+</script>
+
+<h1 on:click={handleClick}>This has been clicked {count} times!</h1>
+
+<!-- European Union Public License version 1.2 -->
+<!-- Copyright © 2020 Rick Beerendonk -->
