ab6035fc81626b9a8e84ca488089fa44a68d77f8
Rick Beerendonk <rick@beerendonk.com>
2020-02-12 07:38:29 +0100

    Add "Reactivity - Arrays - No Issue" example.

---
+++ 06 - Reactivity - TODO/4. Arrays - i. No Issue/src/App.svelte
@@ -0,0 +1,24 @@
+<script>
+  let arr = [1, 2];
+
+  function handleClick() {
+    arr[0] = arr[1] + 1;
+    arr[1] = arr[0] + 2;
+  }
+</script>
+
+<style>
+  .comment {
+    color: gray;
+  }
+</style>
+
+<p class="comment">
+  Observe that clicking does update the array. As long as the array variable
+  name is on the left side of the assignment, reactivity works.
+</p>
+<button on:click={handleClick}>Change array</button>
+<p>{JSON.stringify(arr)}</p>
+
+<!-- European Union Public License version 1.2 -->
+<!-- Copyright © 2020 Rick Beerendonk -->
