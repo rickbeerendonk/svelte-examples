63a786902662d06632378d3d98b1189dc81fd22b
Rick Beerendonk <rick@beerendonk.com>
2020-02-12 21:11:15 +0100

    Add "Bindings - Inside Each" example.

---
+++ 07 - Bindings - TODO/6. Inside Each/src/App.svelte
@@ -0,0 +1,20 @@
+<script>
+  let items = [0, 0, 0];
+</script>
+
+{#each items as item, index}
+  <label>
+    {index + 1}:
+    <!-- Bindings work inside {#each} -->
+    <input type="range" bind:value={item} />
+  </label>
+{/each}
+
+<ul>
+  {#each items as item}
+    <li>{item}</li>
+  {/each}
+</ul>
+
+<!-- European Union Public License version 1.2 -->
+<!-- Copyright © 2020 Rick Beerendonk -->
