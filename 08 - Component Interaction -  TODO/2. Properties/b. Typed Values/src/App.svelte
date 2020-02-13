fc5459c2b7e7c1c5f3aa1742c1de5d4237d41d06
Rick Beerendonk <rick@beerendonk.com>
2020-02-13 21:02:07 +0100

    Add "Component Interactions - Properties - Typed Values" example.

---
+++ 08 - Component Interaction -  TODO/2. Properties/b. Typed Values/src/App.svelte
@@ -0,0 +1,19 @@
+<script>
+  import Child from './Child';
+</script>
+
+<style>
+  .comment {
+    color: gray;
+  }
+</style>
+
+<Child boolProp={true} numberProp={123} stringProp="text" />
+
+<hr />
+<p class="comment">Observe the types are all strings:</p>
+
+<Child boolProp="true" numberProp="123" stringProp="text" />
+
+<!-- European Union Public License version 1.2 -->
+<!-- Copyright © 2020 Rick Beerendonk -->
---
+++ 08 - Component Interaction -  TODO/2. Properties/b. Typed Values/src/Child.svelte
@@ -0,0 +1,26 @@
+<script>
+  export let boolProp;
+  export let numberProp;
+  export let stringProp;
+</script>
+
+<ul>
+  <li>
+    Bool:
+    <b>boolProp</b>
+    ({typeof boolProp})
+  </li>
+  <li>
+    Number:
+    <b>numberProp</b>
+    ({typeof numberProp})
+  </li>
+  <li>
+    String:
+    <b>stringProp</b>
+    ({typeof stringProp})
+  </li>
+</ul>
+
+<!-- European Union Public License version 1.2 -->
+<!-- Copyright © 2020 Rick Beerendonk -->
