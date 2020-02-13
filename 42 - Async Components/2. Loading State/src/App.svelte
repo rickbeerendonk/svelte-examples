2947ee1a4640a1a0a26b9e713523a133059ebd62
Rick Beerendonk <rick@beerendonk.com>
2020-02-13 19:41:52 +0100

    Add "Async Components - Loading State" example.

---
+++ 42 - Async Components/2. Loading State/src/App.svelte
@@ -0,0 +1,43 @@
+<script>
+  import Error from './components/Error';
+  import Loading from './components/Loading';
+
+  const languages = ['Dutch', 'English', 'Spanish', 'Unknown'];
+
+  let selectedLanguage = null;
+  let selectedComponent = null;
+
+  $: {
+    if (selectedLanguage) {
+      selectedComponent = Loading;
+      import(`./components/Greeting${selectedLanguage}.svelte`)
+        .then(module => {
+          selectedComponent = module.default;
+        })
+        .catch(err => {
+          selectedComponent = Error;
+        });
+    }
+  }
+</script>
+
+<style>
+  #result {
+    background: bisque;
+  }
+</style>
+
+{#each languages as language}
+  <label>
+    <input bind:group={selectedLanguage} type="radio" value={language} />
+    {language}
+  </label>
+{/each}
+
+<div id="result">
+  <!-- If this is falsy, no component is shown -->
+  <svelte:component this={selectedComponent} />
+</div>
+
+<!-- European Union Public License version 1.2 -->
+<!-- Copyright © 2020 Rick Beerendonk -->
---
+++ 42 - Async Components/2. Loading State/src/components/Error.svelte
@@ -0,0 +1,10 @@
+<style>
+  h1 {
+    color: red;
+  }
+</style>
+
+<h1>Error</h1>
+
+<!-- European Union Public License version 1.2 -->
+<!-- Copyright © 2020 Rick Beerendonk -->
