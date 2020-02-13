7a3e47bdea98e16612171bb20f2708a1fa352e32
Rick Beerendonk <rick@beerendonk.com>
2020-02-13 20:51:20 +0100

    Add "Component Interactions - Properties - All" example.

---
+++ 08 - Component Interaction -  TODO/2. Properties/e. All/src/App.svelte
@@ -0,0 +1,8 @@
+<script>
+  import Child from './Child';
+</script>
+
+<Child prop1={true} prop2="two" prop3={[1, 2, 3]} />
+
+<!-- European Union Public License version 1.2 -->
+<!-- Copyright © 2020 Rick Beerendonk -->
---
+++ 08 - Component Interaction -  TODO/2. Properties/e. All/src/Child.svelte
@@ -0,0 +1,5 @@
+<!-- Avoid $$props if possible. Hard for Svelte to optimize! -->
+<h1>{JSON.stringify($$props)}!</h1>
+
+<!-- European Union Public License version 1.2 -->
+<!-- Copyright © 2020 Rick Beerendonk -->
