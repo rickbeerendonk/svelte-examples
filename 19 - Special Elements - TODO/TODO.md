2734571633c8abf8ff906586e98ce8be4d60af2d
Rick Beerendonk <rick@beerendonk.com>
2020-02-13 11:21:32 +0100

    Add "Dynamic Components - Basics" example.

--- 19 - Special Elements - TODO/TODO.md
+++ 19 - Special Elements - TODO/TODO.md
@@ -1,1 +1,3 @@
 <svelte:self> is in Recursive Components.
+
+<svelte:component> is in Dynamic Components.
---
+++ 41 - Dynamic Components/1. Basics/src/App.svelte
@@ -0,0 +1,48 @@
+<script>
+  import GreetingDutch from './components/GreetingDutch';
+  import GreetingEnglish from './components/GreetingEnglish';
+  import GreetingSpanish from './components/GreetingSpanish';
+
+  let language = 'english';
+
+  $: greetingComponent = (function() {
+    switch (language) {
+      case 'dutch':
+        return GreetingDutch;
+        break;
+      case 'spanish':
+        return GreetingSpanish;
+        break;
+      default:
+        return GreetingEnglish;
+        break;
+    }
+  })();
+</script>
+
+<style>
+  #result {
+    padding: 4px;
+    background: lightgray;
+  }
+</style>
+
+<label>
+  <input bind:group={language} type="radio" value="dutch" />
+  Dutch
+</label>
+<label>
+  <input bind:group={language} type="radio" value="english" />
+  English
+</label>
+<label>
+  <input bind:group={language} type="radio" value="spanish" />
+  Spanish
+</label>
+
+<div id="result">
+  <svelte:component this={greetingComponent} />
+</div>
+
+<!-- European Union Public License version 1.2 -->
+<!-- Copyright © 2020 Rick Beerendonk -->
