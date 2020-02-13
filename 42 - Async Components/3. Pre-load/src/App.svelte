483cefff247fd90913edad113287652baf253f3b
Rick Beerendonk <rick@beerendonk.com>
2020-02-13 20:40:34 +0100

    Add "Async Components - Pre-load" example.

---
+++ 42 - Async Components/3. Pre-load/src/App.svelte
@@ -0,0 +1,49 @@
+<script>
+  import { onMount } from 'svelte';
+
+  import Error from './components/Error';
+  import Loading from './components/Loading';
+
+  const languages = ['Dutch', 'English', 'Spanish', 'Unknown'];
+
+  let selectedLanguage = null;
+  let selectedComponent = null;
+
+  $: {
+    if (selectedLanguage) {
+      selectedComponent = Loading;
+      import(`./components/Greeting${selectedLanguage}.svelte`)
+        .then(module => {
+          selectedComponent = module.default;
+        })
+        .catch(err => {
+          selectedComponent = Error;
+        });
+    }
+  }
+
+  onMount(async () => {
+    import(`./components/GreetingDutch.svelte`);
+  });
+</script>
+
+<style>
+  #result {
+    background: bisque;
+  }
+</style>
+
+{#each languages as language}
+  <label>
+    <input bind:group={selectedLanguage} type="radio" value={language} />
+    {language}
+  </label>
+{/each}
+
+<div id="result">
+  <!-- If this is falsy, no component is shown -->
+  <svelte:component this={selectedComponent} />
+</div>
+
+<!-- European Union Public License version 1.2 -->
+<!-- Copyright © 2020 Rick Beerendonk -->
---
+++ 42 - Async Components/3. Pre-load/src/components/GreetingSpanish.svelte
@@ -0,0 +1,12 @@
+<script>
+  let greeting = 'Mundo';
+</script>
+
+<label>
+  Spanish:
+  <input bind:value={greeting} />
+</label>
+<p>Hola {greeting}</p>
+
+<!-- European Union Public License version 1.2 -->
+<!-- Copyright © 2020 Rick Beerendonk -->
