13a568ae4686db6a26a4d0eaba8b163f7fcf4ead
Rick Beerendonk <rick@beerendonk.com>
2020-02-13 23:34:51 +0100

    Add "DOM - This - Basics" example.

---
+++ 30 - DOM/1. This - Basics/src/App.svelte
@@ -0,0 +1,14 @@
+<script>
+  import { onMount } from 'svelte';
+
+  let ref;
+
+  onMount(() => {
+    ref.innerHTML = 'Hello <u>Svelte</u>!!!';
+  });
+</script>
+
+<h1 bind:this={ref}>Hello World!</h1>
+
+<!-- European Union Public License version 1.2 -->
+<!-- Copyright © 2020 Rick Beerendonk -->
