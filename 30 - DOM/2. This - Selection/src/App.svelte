8a4d12b2ffbf945bccdf3e83d009f367971a6d22
Rick Beerendonk <rick@beerendonk.com>
2020-02-13 23:42:51 +0100

    Add "DOM - This - Selection" example.

---
+++ 30 - DOM/2. This - Selection/src/App.svelte
@@ -0,0 +1,16 @@
+<script>
+  import { onMount } from 'svelte';
+
+  let name = 'World';
+  let ref;
+
+  onMount(() => {
+    ref.focus();
+  });
+</script>
+
+<input bind:value={name} bind:this={ref} />
+<h1>Hello {name}!</h1>
+
+<!-- European Union Public License version 1.2 -->
+<!-- Copyright © 2020 Rick Beerendonk -->
