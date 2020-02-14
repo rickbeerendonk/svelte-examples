242d8098d44ab1bcdf8580901059733d14e86533
Rick Beerendonk <rick@beerendonk.com>
2020-02-14 21:27:09 +0100

    Add "Motion - Tweened - Update - Shortcut" example.

---
+++ 12 - Motion - TODO/1. Tweened - TODO/b. Update - i. Shortcut/src/App.svelte
@@ -0,0 +1,16 @@
+<script>
+  import { tweened } from 'svelte/motion';
+
+  let count = tweened(0);
+
+  function handleClick() {
+    // Update (Read + Write)
+    // Shortcut for count.update(n => n + 1)
+    $count++;
+  }
+</script>
+
+<h1 on:click={handleClick}>This has been clicked {$count} times!</h1>
+
+<!-- European Union Public License version 1.2 -->
+<!-- Copyright © 2020 Rick Beerendonk -->
