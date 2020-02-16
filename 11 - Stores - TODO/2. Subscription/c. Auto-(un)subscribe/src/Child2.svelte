79e1bc894d71d5bd3617e38cf5e0ada2fc6cbf6d
Rick Beerendonk <rick@beerendonk.com>
2020-02-16 20:40:57 +0100

    Add "Stores - Subscription - Auto-(un)subscribe" example.

---
+++ 11 - Stores - TODO/2. Subscription/c. Auto-(un)subscribe/src/Child2.svelte
@@ -0,0 +1,10 @@
+<script>
+  import { onDestroy } from 'svelte';
+  import { name } from './stores';
+</script>
+
+<!-- Use $<storename> to auto-(un)subscribe. -->
+<h1>Hello {$name}!</h1>
+
+<!-- European Union Public License version 1.2 -->
+<!-- Copyright © 2020 Rick Beerendonk -->
---
+++ 11 - Stores - TODO/2. Subscription/c. Auto-(un)subscribe/webpack.config.js
@@ -0,0 +1,64 @@
+/*! European Union Public License version 1.2 !*/
+/*! Copyright © 2020 Rick Beerendonk          !*/
+
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+const mode = process.env.NODE_ENV || 'development';
+const prod = mode === 'production';
+
+module.exports = {
+  entry: {
+    bundle: ['./src/main.js']
+  },
+  resolve: {
+    extensions: ['.mjs', '.js', '.svelte']
+  },
+  output: {
+    path: __dirname + '/dist',
+    filename: '[name].js',
+    chunkFilename: '[name].[id].js'
+  },
+  devtool: prod ? false : 'source-map',
+  devServer: {
+    contentBase: './dist',
+    // hot: true, // Bug, so don't use
+    port: 9100
+  },
+  mode,
+  module: {
+    rules: [
+      {
+        test: /\.svelte$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'svelte-loader',
+          options: {
+            dev: true,
+            emitCss: true,
+            hotReload: true
+          }
+        }
+      },
+      {
+        test: /\.css$/,
+        use: [
+          /**
+           * MiniCssExtractPlugin doesn't support HMR.
+           * For developing, use 'style-loader' instead.
+           * */
+          prod ? MiniCssExtractPlugin.loader : 'style-loader',
+          'css-loader'
+        ]
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'Stores - Subscription - Auto-(un)subscribe'
+    }),
+    new MiniCssExtractPlugin({
+      filename: '[name].css'
+    })
+  ]
+};
