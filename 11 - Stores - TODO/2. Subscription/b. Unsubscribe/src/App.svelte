212df26e366002bfef0c20e96d0b0c647966c309
Rick Beerendonk <rick@beerendonk.com>
2020-02-16 20:35:22 +0100

    Add "Stores - Subscription - Unsubscribe" example.

---
+++ 11 - Stores - TODO/2. Subscription/b. Unsubscribe/src/App.svelte
@@ -0,0 +1,28 @@
+<script>
+  import { tick } from 'svelte';
+
+  import Child1 from './Child1.svelte';
+  import Child2 from './Child2.svelte';
+
+  let visible = true;
+
+  function handleClick() {
+    visible = !visible;
+  }
+</script>
+
+<div>
+  <button on:click={handleClick}>
+    {#if visible}Hide{:else}Show{/if}
+  </button>
+  <hr />
+  <div>
+    <Child1 />
+    {#if visible}
+      <Child2 />
+    {/if}
+  </div>
+</div>
+
+<!-- European Union Public License version 1.2 -->
+<!-- Copyright © 2020 Rick Beerendonk -->
---
+++ 11 - Stores - TODO/2. Subscription/b. Unsubscribe/src/Child1.svelte
@@ -0,0 +1,12 @@
+<script>
+  import { name } from './stores';
+
+  function changed(e) {
+    name.set(e.target.value);
+  }
+</script>
+
+<input on:input={changed} />
+
+<!-- European Union Public License version 1.2 -->
+<!-- Copyright © 2020 Rick Beerendonk -->
