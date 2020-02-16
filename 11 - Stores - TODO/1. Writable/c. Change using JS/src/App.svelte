14e6da2fce8fda52355cb79e46b3b72ebe0aa781
Rick Beerendonk <rick@beerendonk.com>
2020-02-16 17:39:02 +0100

    Add "Stores - Writable - Change using JS" example.

---
+++ 11 - Stores - TODO/1. Writable/c. Change using JS/src/App.svelte
@@ -0,0 +1,14 @@
+<script>
+  import { name as storeName } from './stores';
+
+  let name = 0;
+
+  storeName.subscribe(value => (name = value));
+</script>
+
+<div>
+  <h1>Hello {name}!</h1>
+</div>
+
+<!-- European Union Public License version 1.2 -->
+<!-- Copyright © 2020 Rick Beerendonk -->
---
+++ 11 - Stores - TODO/1. Writable/c. Change using JS/src/main.js
@@ -0,0 +1,11 @@
+/*! European Union Public License version 1.2 !*/
+/*! Copyright © 2020 Rick Beerendonk          !*/
+
+import App from './App';
+import {} from './code';
+
+const app = new App({
+  target: document.body
+});
+
+export default app;
