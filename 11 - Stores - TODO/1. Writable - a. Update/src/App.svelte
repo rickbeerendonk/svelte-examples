97f4245b16b427490245705b2a3a229a6d84185f
Rick Beerendonk <rick@beerendonk.com>
2020-02-16 17:11:39 +0100

    Add "Stores - Writable - Update" example.

---
+++ 11 - Stores - TODO/1. Writable - a. Update/src/App.svelte
@@ -0,0 +1,16 @@
+<script>
+  import Child from './Child.svelte';
+  import { count as storeCount } from './stores';
+
+  let count = 0;
+
+  storeCount.subscribe(newValue => (count = newValue));
+</script>
+
+<div>
+  <Child />
+  <h1>Clicked: {count}</h1>
+</div>
+
+<!-- European Union Public License version 1.2 -->
+<!-- Copyright © 2020 Rick Beerendonk -->
