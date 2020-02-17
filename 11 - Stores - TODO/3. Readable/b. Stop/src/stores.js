985b88bf2a12dd816b4dd1f6a3bd7bb6b83504fd
Rick Beerendonk <rick@beerendonk.com>
2020-02-17 09:47:25 +0100

    Add "Stores - Readable - Stop" example.

---
+++ 11 - Stores - TODO/3. Readable/b. Stop/src/stores.js
@@ -0,0 +1,24 @@
+/*! European Union Public License version 1.2 !*/
+/*! Copyright © 2020 Rick Beerendonk          !*/
+
+/* eslint no-console:"off" */
+
+import { readable } from 'svelte/store';
+
+export const time = readable(
+  // Initial value
+  undefined,
+  // Start setting values
+  function(set) {
+    console.log('▶ Start readable store');
+
+    // Internal set
+    const intervalId = setInterval(() => set(new Date()), 5000);
+
+    // Stop setting values when there are no listeners
+    return function() {
+      clearInterval(intervalId);
+      console.log('◀ Stop readable store');
+    };
+  }
+);
