df7ea965035866e511f1d847033f37427ce545eb
Rick Beerendonk <rick@beerendonk.com>
2020-02-17 22:50:31 +0100

    Add "Bindings - This - Basics" example.

---
+++ 07 - Bindings - TODO/8. This/a. Basics/src/App.svelte
@@ -0,0 +1,12 @@
+<script>
+  import { onMount } from 'svelte';
+
+  let myH1;
+
+  onMount(() => (myH1.innerHTML = 'Hello <u>Svelte</u>!!!'));
+</script>
+
+<h1 bind:this={myH1}>Hello World!</h1>
+
+<!-- European Union Public License version 1.2 -->
+<!-- Copyright © 2020 Rick Beerendonk -->
