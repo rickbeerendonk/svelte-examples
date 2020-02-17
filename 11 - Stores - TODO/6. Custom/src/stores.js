d2cff97017fb91f7bb8510a0a40028f49cb669b8
Rick Beerendonk <rick@beerendonk.com>
2020-02-17 22:03:44 +0100

    Add "Stores - Custom" example.

---
+++ 11 - Stores - TODO/6. Custom/src/stores.js
@@ -0,0 +1,16 @@
+import { writable } from 'svelte/store';
+
+function createCount() {
+  const initial = 0;
+
+  const { subscribe, set, update } = writable(initial);
+
+  return {
+    subscribe,
+    inc: () => update(value => value + 1),
+    dec: (delta = 1) => update(value => value - delta),
+    reset: () => set(initial)
+  };
+}
+
+export const count = createCount();
