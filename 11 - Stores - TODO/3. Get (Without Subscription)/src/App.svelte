f24552f169bc26161da996e625811ac917ac0b6d
Rick Beerendonk <rick@beerendonk.com>
2020-02-17 17:28:21 +0100

    Add "Stores - Subscription - Get (Without Subscription)" example.

---
+++ 11 - Stores - TODO/3. Get (Without Subscription)/src/App.svelte
@@ -0,0 +1,39 @@
+<script>
+  import { tick } from 'svelte';
+
+  import Child_Get from './Child_Get.svelte';
+
+  let visible = true;
+
+  function handleClick() {
+    visible = !visible;
+  }
+</script>
+
+<style>
+  .comment {
+    color: gray;
+  }
+</style>
+
+<div>
+  <p class="comment">
+    Example of getting a value from a store without a subscription.
+  </p>
+  <p class="comment">
+    Open the console to see that in reality there is a subscribe immediately
+    followed by an unsubscribe event. Press the button to call get().
+  </p>
+  <button on:click={handleClick}>
+    {#if visible}Hide{:else}Show{/if}
+  </button>
+  <hr />
+  <div>
+    {#if visible}
+      <Child_Get />
+    {/if}
+  </div>
+</div>
+
+<!-- European Union Public License version 1.2 -->
+<!-- Copyright © 2020 Rick Beerendonk -->
