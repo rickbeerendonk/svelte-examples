eea8ad8f8ecc8e6f136a60038b626d085dbff8ce
Rick Beerendonk <rick@beerendonk.com>
2020-02-18 08:30:26 +0100

    Add "Bindings - Transitions - Basics" example.

---
+++ 13 - Transitions - TODO/a. Basics/src/App.svelte
@@ -0,0 +1,17 @@
+<script>
+  import { fade } from 'svelte/transition';
+
+  let visible = true;
+</script>
+
+<label>
+  <input type="checkbox" bind:checked={visible} />
+  Visible
+</label>
+
+{#if visible}
+  <h1 transition:fade>Hello Svelte!</h1>
+{/if}
+
+<!-- European Union Public License version 1.2 -->
+<!-- Copyright © 2020 Rick Beerendonk -->
