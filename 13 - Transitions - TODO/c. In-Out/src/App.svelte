67b42c8abbbc2fb633a077ae31fa111a885e3c89
Rick Beerendonk <rick@beerendonk.com>
2020-02-18 08:34:45 +0100

    Add "Bindings - Transitions - In-Out" example.

---
+++ 13 - Transitions - TODO/c. In-Out/src/App.svelte
@@ -0,0 +1,19 @@
+<script>
+  import { fade } from 'svelte/transition';
+
+  let visible = true;
+</script>
+
+<label>
+  <input type="checkbox" bind:checked={visible} />
+  Visible
+</label>
+
+{#if visible}
+  <h1 in:fade={{ duration: 3000 }} out:fade={{ duration: 400 }}>
+    Hello Svelte!
+  </h1>
+{/if}
+
+<!-- European Union Public License version 1.2 -->
+<!-- Copyright © 2020 Rick Beerendonk -->
