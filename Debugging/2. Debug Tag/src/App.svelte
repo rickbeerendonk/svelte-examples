db6b307b027dbf80d9b313efbf07a3fe3b429e76
Rick Beerendonk <rick@beerendonk.com>
2020-02-25 20:37:29 +0100

    Add "Debugging - Debug Tag" example.

---
+++ Debugging/2. Debug Tag/src/App.svelte
@@ -0,0 +1,17 @@
+<script>
+  let count = 0;
+  let time = '';
+
+  setInterval(() => {
+    time = Date();
+  }, 1000);
+</script>
+
+<!-- @debug is in a template what console.log() + debugger is in JavaScript -->
+{@debug count, time}
+
+<h1 on:click={() => count++}>This has been clicked {count} times!</h1>
+<h2>{time}</h2>
+
+<!-- European Union Public License version 1.2 -->
+<!-- Copyright © 2020 Rick Beerendonk -->
