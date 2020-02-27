480b4ef1e8728059408cdb2d3e1a0f458f610eeb
Rick Beerendonk <rick@beerendonk.com>
2020-02-27 20:43:14 +0100

    Add "Motion - Spring - Set" example.

---
+++ 12 - Motion - TODO/2. Spring - TODO/a. Set/src/App.svelte
@@ -0,0 +1,19 @@
+<script>
+  import { spring } from 'svelte/motion';
+
+  let count = spring(0);
+
+  function handleClick() {
+    // Read
+    const currentCount = $count;
+
+    // Write
+    count.set(currentCount + 1);
+  }
+</script>
+
+<!-- Use $... to get the value -->
+<h1 on:click={handleClick}>This has been clicked {$count} times!</h1>
+
+<!-- European Union Public License version 1.2 -->
+<!-- Copyright © 2020 Rick Beerendonk -->
