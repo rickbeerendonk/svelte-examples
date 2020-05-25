fceef2ba621b806157c11fe4fb2098b6e24ec61c
Rick Beerendonk <rick@beerendonk.com>
2020-05-25 14:04:08 +0200

    Add "Recursive Components - In Slot" example.

---
+++ 40 - Recursive Components/3. In Slot/src/Container.svelte
@@ -0,0 +1,28 @@
+<script>
+  let recursive = false;
+</script>
+
+<style>
+  .container {
+    background-color: lightgray;
+    padding: 10px;
+  }
+
+  .content {
+    background-color: white;
+    padding: 10px;
+  }
+</style>
+
+<h1 on:click={() => (recursive = true)}>Hello World!</h1>
+{#if recursive}
+  <div class="container">
+    The only item:
+    <div class="content">
+      <slot />
+    </div>
+  </div>
+{/if}
+
+<!-- European Union Public License version 1.2 -->
+<!-- Copyright © 2020 Rick Beerendonk -->
