30bed5573d813eacd97691614f0676a2969fdfc0
Rick Beerendonk <rick@beerendonk.com>
2024-03-15 00:07:51 +0100

    Add "Data Binding - Style - Property" example.

---
+++ 02 - Data Binding/3. Style/b. Property/js/src/App.svelte
@@ -0,0 +1,19 @@
+<script>
+  const isImportant = true;
+</script>
+
+<h1 class:important={isImportant}>Hello Svelte!</h1>
+
+<style>
+  h1 {
+    font-size: 48px;
+    font-style: italic;
+  }
+
+  .important {
+    text-decoration: underline;
+  }
+</style>
+
+<!-- European Union Public License version 1.2 -->
+<!-- Copyright © 2024 Rick Beerendonk -->
