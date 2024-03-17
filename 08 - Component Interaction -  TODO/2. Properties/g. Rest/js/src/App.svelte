1aa746e7fc8f531fc4d6796bd01355125e6bad91
Rick Beerendonk <rick@beerendonk.com>
2024-03-17 17:38:55 +0100

    Add "Component Interaction - Properties - Rest" example.

---
+++ 08 - Component Interaction -  TODO/2. Properties/g. Rest/js/src/App.svelte
@@ -0,0 +1,16 @@
+<script>
+  import ImportantAnchor from './ImportantAnchor.svelte';
+</script>
+
+<ImportantAnchor
+  href="https://svelte.dev"
+  rel="noopener noreferrer"
+  target="_blank"
+  title="Everything about Svelte"
+  important="***"
+>
+  Svelte website
+</ImportantAnchor>
+
+<!-- European Union Public License version 1.2 -->
+<!-- Copyright © 2020 Rick Beerendonk -->
---
+++ 08 - Component Interaction -  TODO/2. Properties/g. Rest/js/src/ImportantAnchor.svelte
@@ -0,0 +1,9 @@
+<script>
+  export let important;
+</script>
+
+<!-- svelte-ignore a11y-missing-attribute -->
+<a {...$$restProps}>{important} <slot /> {important}</a>
+
+<!-- European Union Public License version 1.2 -->
+<!-- Copyright © 2024 Rick Beerendonk -->
