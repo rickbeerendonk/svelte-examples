6828e5d50656c4b1d2ebb08acf72996a45bfc998
Rick Beerendonk <rick@beerendonk.com>
2024-03-17 11:36:30 +0100

    Add "Portals (3rd party) - Action" example.

---
+++ 23 - Portals (3rd party)/b. Action/js/src/App.svelte
@@ -0,0 +1,12 @@
+<script>
+  import { portal } from 'svelte-portal';
+
+  let name = 'Svelte';
+</script>
+
+<h1>Hello {name}!</h1>
+
+<h1 use:portal={'#portal'}>Hello {name} in a Portal!</h1>
+
+<!-- European Union Public License version 1.2 -->
+<!-- Copyright © 2024 Rick Beerendonk -->
