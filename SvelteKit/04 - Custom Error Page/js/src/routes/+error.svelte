acc0f528abe47163f0b272ac719160ce28092c83
Rick Beerendonk <rick@beerendonk.com>
2024-03-17 01:50:40 +0100

    Add "SvelteKit - Custom Error Page" example.

---
+++ SvelteKit/04 - Custom Error Page/js/src/routes/+error.svelte
@@ -0,0 +1,13 @@
+<!-- European Union Public License version 1.2 -->
+<!-- Copyright © 2024 Rick Beerendonk          -->
+
+<script>
+  import { page } from '$app/stores';
+</script>
+
+<h1>Custom Error Message</h1>
+
+<ul>
+  <li>Status: {$page.status}</li>
+  <li>Error Message: {$page.error.message}</li>
+</ul>
---
+++ SvelteKit/04 - Custom Error Page/js/src/routes/+layout.svelte
@@ -0,0 +1,40 @@
+<!-- European Union Public License version 1.2 -->
+<!-- Copyright © 2024 Rick Beerendonk          -->
+
+<header>
+  <h1>App</h1>
+  <nav>
+    <a href="/">Home</a>
+    <a href="page1">Page 1</a>
+    <a href="page2">Page 2</a>
+    <a href="page999">Page 999</a>
+  </nav>
+</header>
+
+<main>
+  <slot />
+</main>
+
+<footer>My copyright</footer>
+
+<style>
+  header,
+  footer {
+    background: lightblue;
+    padding: 0.5rem;
+  }
+
+  main {
+    padding: 0.5rem;
+  }
+
+  header h1,
+  header nav {
+    margin-block-start: 0;
+    margin-block-end: 0;
+  }
+
+  header nav > a {
+    margin-right: 0.5rem;
+  }
+</style>
