e890b94d1c39eed3f8211043d312753121aa9bae
Rick Beerendonk <rick@beerendonk.com>
2024-03-17 01:00:34 +0100

    Add "Data Binding - View to Data - Debug" example.

---
+++ 02 - Data Binding/2. View to Data/d. Debug/js/src/App.svelte
@@ -0,0 +1,13 @@
+<script>
+  /* eslint-disable svelte/no-at-debug-tags */
+
+  let count = 0;
+</script>
+
+<!-- Will not work with properties of objects or array entries -->
+{@debug count}
+
+<button on:click={() => count++}>This has been clicked {count} times!</button>
+
+<!-- European Union Public License version 1.2 -->
+<!-- Copyright © 2024 Rick Beerendonk -->
