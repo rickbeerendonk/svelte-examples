a4a4b53269eacd132a6669bd6d4ca7b5db6fda76
Rick Beerendonk <rick@beerendonk.com>
2024-03-17 19:01:52 +0100

    Add "Data Binding - Data to View - Content - Expression" example.

---
+++ 02 - Data Binding/1. Data to View/a. Content - ii. Expression/js/src/App.svelte
@@ -0,0 +1,9 @@
+<script>
+  let name = 'Svelte';
+  let year = 2024;
+</script>
+
+<h1>Hello {name.toUpperCase()} in {year + 1}!</h1>
+
+<!-- European Union Public License version 1.2 -->
+<!-- Copyright © 2024 Rick Beerendonk -->
