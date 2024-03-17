45fc567489b3786f8dc1af3c6a1b9a0699b71f44
Rick Beerendonk <rick@beerendonk.com>
2024-03-17 18:51:41 +0100

    Add "Render Root - Multiple Tags" example.

---
+++ 01 - Render Root/2. Multiple Tags/js/src/App.svelte
@@ -0,0 +1,7 @@
+<!-- European Union Public License version 1.2 -->
+<!-- Copyright © 2024 Rick Beerendonk -->
+
+<!-- No issue to have multiple tags at the component root -->
+<h1>One</h1>
+<h2>Two</h2>
+<h3>Three</h3>
