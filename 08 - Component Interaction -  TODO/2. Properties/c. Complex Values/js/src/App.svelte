49d4992d82b5cac0df156b31c815851e329a9cb7
Rick Beerendonk <rick@beerendonk.com>
2024-03-18 05:06:53 +0100

    Add "Component Interaction - Properties - Complex Values" example.

---
+++ 08 - Component Interaction -  TODO/2. Properties/c. Complex Values/js/src/App.svelte
@@ -0,0 +1,8 @@
+<!-- European Union Public License version 1.2 -->
+<!-- Copyright © 2024 Rick Beerendonk -->
+
+<script>
+  import Child from './Child.svelte';
+</script>
+
+<Child arrayProp={[1, 2]} objectProp={{ a: 1, b: 2 }} />
---
+++ 08 - Component Interaction -  TODO/2. Properties/c. Complex Values/js/src/Child.svelte
@@ -0,0 +1,12 @@
+<!-- European Union Public License version 1.2 -->
+<!-- Copyright © 2024 Rick Beerendonk -->
+
+<script>
+  export let arrayProp;
+  export let objectProp;
+</script>
+
+<ul>
+  <li>Array: {JSON.stringify(arrayProp)}</li>
+  <li>Object: {JSON.stringify(objectProp)}</li>
+</ul>
