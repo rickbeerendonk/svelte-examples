dab9d30728bcac4b78638c99d20d01f141fafef0
Rick Beerendonk <rick@beerendonk.com>
2024-03-18 23:03:31 +0100

    Add "Data Binding - Data to View - Attribute - Partial" example.

---
+++ 02 - Data Binding/1. Data to View/b. Attribute - v. Partial/js/src/App.svelte
@@ -0,0 +1,9 @@
+<!-- European Union Public License version 1.2 -->
+<!-- Copyright © 2024 Rick Beerendonk -->
+
+<script>
+  let name = 'Svelte';
+</script>
+
+<!-- Partial databinding (in a string!)-->
+<input value="Hello {name}!" />
