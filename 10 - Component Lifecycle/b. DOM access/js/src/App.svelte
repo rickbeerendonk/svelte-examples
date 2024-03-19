2d351246482b14c4c1bdb70da68023f9c0964c1b
Rick Beerendonk <rick@beerendonk.com>
2024-03-19 10:11:47 +0100

    Add "DOM Access" example.

---
+++ 10 - Component Lifecycle/b. DOM access/js/src/App.svelte
@@ -0,0 +1,20 @@
+<!-- European Union Public License version 1.2 -->
+<!-- Copyright © 2024 Rick Beerendonk -->
+
+<script>
+  import { onMount } from 'svelte';
+
+  let name = 'World';
+  let inputElement;
+
+  onMount(() => {
+    inputElement.select();
+  });
+
+  function handleChange(event) {
+    name = event.target.value;
+  }
+</script>
+
+<input bind:this={inputElement} value={name} on:input={handleChange} />
+<p>Hello {name}!</p>
