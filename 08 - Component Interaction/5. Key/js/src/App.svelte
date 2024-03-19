fc1f923cc056be454f75f724339ad0afec43c688
Rick Beerendonk <rick@beerendonk.com>
2024-03-19 21:47:28 +0100

    Add "Component Interaction - Key" example.

---
+++ 08 - Component Interaction/5. Key/js/src/App.svelte
@@ -0,0 +1,18 @@
+<!-- European Union Public License version 1.2 -->
+<!-- Copyright © 2024 Rick Beerendonk -->
+
+<script>
+  import ClickMe from './ClickMe.svelte';
+
+  let id = 0;
+
+  function handleClick() {
+    id += 1;
+  }
+</script>
+
+<button on:click={handleClick}>Change ID</button>
+<br />
+{#key id}
+  <ClickMe />
+{/key}
---
+++ 08 - Component Interaction/5. Key/js/src/ClickMe.svelte
@@ -0,0 +1,14 @@
+<!-- European Union Public License version 1.2 -->
+<!-- Copyright © 2024 Rick Beerendonk -->
+
+<script>
+  let count = 0;
+
+  function handleClick() {
+    count += 1;
+  }
+</script>
+
+<button on:click={handleClick}>
+  This button has been clicked {count} times
+</button>
