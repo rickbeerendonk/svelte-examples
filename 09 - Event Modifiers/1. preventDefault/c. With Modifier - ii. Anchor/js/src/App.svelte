d4ef734aa63365a8978ce419c17d4ae592828f61
Rick Beerendonk <rick@beerendonk.com>
2024-03-19 06:12:54 +0100

    Add Anchor variant.

---
+++ 09 - Event Modifiers/1. preventDefault/c. With Modifier - ii. Anchor/js/src/App.svelte
@@ -0,0 +1,12 @@
+<script>
+  function handleClick(event) {
+    //event.preventDefault();
+
+    alert('Handled in code!');
+  }
+</script>
+
+<a href="https://svelte.dev" on:click|preventDefault={handleClick}>Svelte!</a>
+
+<!-- European Union Public License version 1.2 -->
+<!-- Copyright © 2020 Rick Beerendonk -->
