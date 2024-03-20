1acfe84517f6a46b50408edebf36739a20bd32a6
Rick Beerendonk <rick@beerendonk.com>
2024-03-20 04:29:38 +0100

    Add "SvelteKit - Images" example.

---
+++ SvelteKit/09 - Images/js/src/routes/+page.svelte
@@ -0,0 +1,8 @@
+<!-- European Union Public License version 1.2 -->
+<!-- Copyright © 2024 Rick Beerendonk          -->
+
+<svelte:head>
+  <title>Home</title>
+</svelte:head>
+
+<h2>Home</h2>
---
+++ SvelteKit/09 - Images/js/src/routes/planting/+page.svelte
@@ -0,0 +1,17 @@
+<!-- European Union Public License version 1.2 -->
+<!-- Copyright © 2024 Rick Beerendonk          -->
+
+<script>
+  import planting from '$lib/images/planting.png';
+</script>
+
+<svelte:head>
+  <title>Planting</title>
+</svelte:head>
+
+<h2>Planting</h2>
+
+<figure>
+  <img alt="Planting of bulbs" src={planting} />
+  <figcaption>Planting</figcaption>
+</figure>
