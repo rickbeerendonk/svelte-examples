ba203af6751f5cb637c73ebbfc29c8f4e4bbd4a3
Rick Beerendonk <rick@beerendonk.com>
2024-03-20 12:32:52 +0100

    Add "SvelteKit - Images - Local" example.

---
+++ SvelteKit/08 - Images - ii. Local/js/src/routes/topping/+page.svelte
@@ -0,0 +1,17 @@
+<!-- European Union Public License version 1.2 -->
+<!-- Copyright © 2024 Rick Beerendonk          -->
+
+<script>
+  import topping from './topping.png';
+</script>
+
+<svelte:head>
+  <title>Topping</title>
+</svelte:head>
+
+<h2>Topping</h2>
+
+<figure>
+  <img alt="Topping of tulips" src={topping} />
+  <figcaption>Topping</figcaption>
+</figure>
