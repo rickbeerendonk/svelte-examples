d36ba874f5dfe0df83e76ece1aa6535c288dc400
Rick Beerendonk <rick@beerendonk.com>
2024-03-20 03:06:22 +0100

    Add "Bindings - Handling Forms" example.

---
+++ 07 - Bindings/9. Handling Forms/js/src/App.svelte
@@ -0,0 +1,22 @@
+<!-- European Union Public License version 1.2 -->
+<!-- Copyright © 2024 Rick Beerendonk -->
+
+<script>
+  import ChildWithForm from './ChildWithForm.svelte';
+
+  let items = ['Item 1', 'Item 2'];
+
+  function handleAdd(e) {
+    const newItem = e.detail;
+    items = [...items, newItem]; // Reactive assignment
+  }
+</script>
+
+<div>
+  <ul>
+    {#each items as item}
+      <li>{item}</li>
+    {/each}
+  </ul>
+  <ChildWithForm on:added={handleAdd} />
+</div>
---
+++ 07 - Bindings/9. Handling Forms/js/src/ChildWithForm.svelte
@@ -0,0 +1,19 @@
+<!-- European Union Public License version 1.2 -->
+<!-- Copyright © 2024 Rick Beerendonk -->
+
+<script>
+  import { createEventDispatcher } from 'svelte';
+
+  let name = '';
+  const dispatch = createEventDispatcher();
+
+  function handleSubmit() {
+    dispatch('added', name); // Emit the custom event 'add' with the name as detail
+    name = ''; // Clear the input field
+  }
+</script>
+
+<form on:submit|preventDefault={handleSubmit}>
+  <input bind:value={name} />
+  <button type="submit">Add</button>
+</form>
