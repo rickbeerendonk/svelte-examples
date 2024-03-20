01ed20738237a23ddc47ccd4518780a2ecba880a
Rick Beerendonk <rick@beerendonk.com>
2024-03-20 02:26:07 +0100

    Add "SvelteKit - Dynamic Routing - Multiple Params" example.

---
+++ SvelteKit/05 - Dynamic Routing - b. Multiple Params/js/src/routes/+page.svelte
@@ -0,0 +1,25 @@
+<!-- European Union Public License version 1.2 -->
+<!-- Copyright © 2024 Rick Beerendonk          -->
+
+<script>
+  let size = 4;
+  let color = 'red';
+</script>
+
+<h2>Home</h2>
+
+<label>Size: <input type="number" bind:value={size} /></label>
+<br />
+<label
+  >Color:
+  <select bind:value={color}>
+    <option value="blue">Blue</option>
+    <option value="green">Green</option>
+    <option value="red">Red</option>
+    <option value="yellow">Yellow</option>
+  </select>
+</label>
+
+<hr />
+
+<a href="\shirt\{size}\{color}">{`Go to your ${color} shirt, size ${size}`}</a>
---
+++ SvelteKit/05 - Dynamic Routing - b. Multiple Params/js/src/routes/shirt/[size]/[color]/+page.svelte
@@ -0,0 +1,9 @@
+<!-- European Union Public License version 1.2 -->
+<!-- Copyright © 2024 Rick Beerendonk          -->
+
+<script>
+  import { page } from '$app/stores';
+</script>
+
+<h2>Size: {$page.params.size}</h2>
+<h2>Color: {$page.params.color}</h2>
