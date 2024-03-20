182c9819c731f369d84b6df30cd17a47e68e3e61
Rick Beerendonk <rick@beerendonk.com>
2024-03-20 01:51:53 +0100

    Add "Module Context" example.

---
+++ 20 - Module Context/js/src/App.svelte
@@ -0,0 +1,11 @@
+<!-- European Union Public License version 1.2 -->
+<!-- Copyright © 2024 Rick Beerendonk -->
+
+<script>
+  import Child from './Child.svelte';
+</script>
+
+<h1>App</h1>
+<Child />
+<Child />
+<Child />
---
+++ 20 - Module Context/js/src/Child.svelte
@@ -0,0 +1,13 @@
+<!-- European Union Public License version 1.2 -->
+<!-- Copyright © 2024 Rick Beerendonk -->
+
+<script context="module">
+  // This code runs once (= static), no matter how many <Child> instances exist
+  let staticId = 1;
+</script>
+
+<script>
+  let id = staticId++;
+</script>
+
+<h2>Child {id}</h2>
