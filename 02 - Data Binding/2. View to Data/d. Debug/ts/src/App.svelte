566c2cdf9d12cddc74065c09274c58d61058ddb5
Rick Beerendonk <rick@beerendonk.com>
2026-01-29 12:06:44 -0500

    Add TS version.

---
+++ 02 - Data Binding/2. View to Data/d. Debug/ts/src/App.svelte
@@ -0,0 +1,27 @@
+<!-- European Union Public License version 1.2 -->
+<!-- Copyright © 2024 Rick Beerendonk -->
+
+<script>
+  /* eslint-disable svelte/no-at-debug-tags */
+
+  let count = $state(0);
+  let obj = $state({ count: 0 });
+  let arr = $state([0]);
+</script>
+
+<!-- @debug is in a template what console.log() + debugger is in JavaScript -->
+{@debug count}
+{@debug obj}
+{@debug arr}
+
+<button onclick={() => count++}>
+  Value: This has been clicked {count} times!
+</button>
+
+<button onclick={() => obj.count++}>
+  Object: This has been clicked {obj.count} times!
+</button>
+
+<button onclick={() => arr[0]++}>
+  Array: This has been clicked {arr[0]} times!
+</button>
