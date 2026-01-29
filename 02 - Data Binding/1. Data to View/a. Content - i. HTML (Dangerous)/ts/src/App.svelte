67e6a010a973383001801fb95d40df75e2f701b1
Rick Beerendonk <rick@beerendonk.com>
2026-01-29 10:38:03 -0500

    Add TS version.

---
+++ 02 - Data Binding/1. Data to View/a. Content - i. HTML (Dangerous)/ts/src/App.svelte
@@ -0,0 +1,18 @@
+<!-- European Union Public License version 1.2 -->
+<!-- Copyright © 2020 Rick Beerendonk -->
+
+<script>
+  /* eslint-disable svelte/no-at-html-tags */
+
+  const name = '<u>Svelte</u>';
+  //const name = '<u onmouseover=alert("Hacked!")>Dangerous</u>';
+</script>
+
+<h1>
+  <!-- Sanitize before using {@html} -->
+  Escaping: Hello
+  {@html name}
+  !
+</h1>
+
+<h1>NOT escaping: Hello {name} !</h1>
