7cf55b6a55a7b8c8412e979ee687690aebab6997
Rick Beerendonk <rick@beerendonk.com>
2026-01-29 12:13:28 -0500

    Update to Svelte 5.

--- 02 - Data Binding/3. Style/b. Inline - ii. style: directive/js/src/App.svelte
+++ 02 - Data Binding/3. Style/b. Inline - ii. style: directive/js/src/App.svelte
@@ -1,6 +1,7 @@
 <!-- European Union Public License version 1.2 -->
 <!-- Copyright © 2024 Rick Beerendonk -->
 
+<!-- style: directive applies inline styles to an element -->
 <h1
   style:text-decoration="underline"
   style:font-style="italic"
