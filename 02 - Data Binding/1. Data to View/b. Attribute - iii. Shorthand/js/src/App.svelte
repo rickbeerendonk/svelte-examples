f995a04b7996d7364d8c1903bd37eb36c061aa74
Rick Beerendonk <rick@beerendonk.com>
2026-01-29 10:43:12 -0500

    Update to Svelte 5.

--- 02 - Data Binding/1. Data to View/b. Attribute - iii. Shorthand/js/src/App.svelte
+++ 02 - Data Binding/1. Data to View/b. Attribute - iii. Shorthand/js/src/App.svelte
@@ -1,8 +1,8 @@
+<!-- European Union Public License version 1.2 -->
+<!-- Copyright © 2020 Rick Beerendonk -->
+
 <script>
   let value = 'Svelte';
 </script>
 
 <input {value} />
-
-<!-- European Union Public License version 1.2 -->
-<!-- Copyright © 2020 Rick Beerendonk -->
