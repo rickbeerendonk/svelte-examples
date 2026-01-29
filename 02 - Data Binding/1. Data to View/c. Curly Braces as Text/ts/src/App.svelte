19125c8140badacc61a386dce24bbe8235ce47a9
Rick Beerendonk <rick@beerendonk.com>
2026-01-29 14:09:45 -0500

    Fix copyright location.

--- 02 - Data Binding/1. Data to View/c. Curly Braces as Text/ts/src/App.svelte
+++ 02 - Data Binding/1. Data to View/c. Curly Braces as Text/ts/src/App.svelte
@@ -1,8 +1,8 @@
+<!-- European Union Public License version 1.2 -->
+<!-- Copyright © 2020 Rick Beerendonk -->
+
 <h1>Hello {'{}'}!</h1>
 
 <h1>Hello {'{'}}!</h1>
 
 <h1>Hello &#123;}!</h1>
-
-<!-- European Union Public License version 1.2 -->
-<!-- Copyright © 2020 Rick Beerendonk -->
