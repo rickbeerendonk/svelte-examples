3410573f6a73849948ce6f80fcc20a0316115794
Rick Beerendonk <rick@beerendonk.com>
2026-01-29 10:35:21 -0500

    Add TS version.

---
+++ 02 - Data Binding/1. Data to View/a. Content - ii. Expression/ts/src/App.svelte
@@ -0,0 +1,9 @@
+<!-- European Union Public License version 1.2 -->
+<!-- Copyright © 2024 Rick Beerendonk -->
+
+<script>
+  const name = 'Svelte';
+  const year = 2024;
+</script>
+
+<h1>Hello {name.toUpperCase()} in {year + 1}!</h1>
