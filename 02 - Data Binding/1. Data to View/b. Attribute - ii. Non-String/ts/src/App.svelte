4ebe350535dcb0360ab4937ad262d2e80ace7b72
Rick Beerendonk <rick@beerendonk.com>
2026-01-29 10:40:30 -0500

    Add TS version.

---
+++ 02 - Data Binding/1. Data to View/b. Attribute - ii. Non-String/ts/src/App.svelte
@@ -0,0 +1,8 @@
+<!-- European Union Public License version 1.2 -->
+<!-- Copyright © 2024 Rick Beerendonk -->
+
+<!-- Fixed -->
+<input type="button" value="Action" disabled />
+
+<!-- Flexible -->
+<input type="button" value="Action" disabled={true} />
