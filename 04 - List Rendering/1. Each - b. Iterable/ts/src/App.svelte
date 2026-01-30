41e6175616565a2619c877803666617ad7591c5d
Rick Beerendonk <rick@beerendonk.com>
2026-01-30 09:20:49 -0500

    Add TS version.

---
+++ 04 - List Rendering/1. Each - b. Iterable/ts/src/App.svelte
@@ -0,0 +1,13 @@
+<!-- European Union Public License version 1.2 -->
+<!-- Copyright © 2020 Rick Beerendonk -->
+
+<script lang="ts">
+  const items = new Set(['A', 'B', 'C']);
+</script>
+
+<ul>
+  <!-- eslint-disable-next-line svelte/require-each-key -->
+  {#each items as item}
+    <li>{item}</li>
+  {/each}
+</ul>
