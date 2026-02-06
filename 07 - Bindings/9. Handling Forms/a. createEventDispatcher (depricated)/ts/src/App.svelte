50790bbc4bd1646d61a17e240d3a3e584b111c33
Rick Beerendonk <rick@beerendonk.com>
2026-02-06 09:52:04 -0500

    Add TS version. Mark as deprecated.

---
+++ 07 - Bindings/9. Handling Forms/a. createEventDispatcher (depricated)/ts/src/App.svelte
@@ -0,0 +1,26 @@
+<!-- European Union Public License version 1.2 -->
+<!-- Copyright © 2024 Rick Beerendonk -->
+
+<!-- eslint-disable svelte/require-each-key -->
+<!-- No key needed for this basic form handling educational example -->
+
+<script lang="ts">
+  import ChildWithForm from './ChildWithForm.svelte';
+
+  let items = ['Item 1', 'Item 2'];
+
+  function handleAdd(e: CustomEvent<string>) {
+    const newItem = e.detail;
+    items = [...items, newItem]; // Reactive assignment
+  }
+</script>
+
+<div>
+  <ul>
+    <!-- eslint-disable-next-line svelte/require-each-key -->
+    {#each items as item}
+      <li>{item}</li>
+    {/each}
+  </ul>
+  <ChildWithForm on:added={handleAdd} />
+</div>
