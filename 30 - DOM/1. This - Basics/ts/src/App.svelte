561d15a86218c3d0eec049c12ba77cfec90180d1
Rick Beerendonk <rick@beerendonk.com>
2026-02-06 09:37:59 -0500

    Update to Svelte 5.

--- 30 - DOM/1. This - Basics/ts/src/App.svelte
+++ 30 - DOM/1. This - Basics/ts/src/App.svelte
@@ -1,14 +1,22 @@
+<!-- European Union Public License version 1.2 -->
+<!-- Copyright © 2020 Rick Beerendonk -->
+
 <script lang="ts">
+  /* eslint-disable svelte/no-dom-manipulating */
+  // DOM manipulation is intentional for educational purposes
+
   import { onMount } from 'svelte';
 
-  let ref;
+  let ref: HTMLHeadingElement | undefined;
 
   onMount(() => {
-    ref.innerHTML = 'Hello <u>Svelte</u>!!!';
+    if (ref) {
+      // Don't manipulate the DOM directly.
+      // The Svelte runtime can get confused if there is a difference
+      // between the actual DOM and the DOM expected by the Svelte runtime.
+      ref.innerHTML = 'Hello <u>Svelte</u>!!!';
+    }
   });
 </script>
 
 <h1 bind:this={ref}>Hello World!</h1>
-
-<!-- European Union Public License version 1.2 -->
-<!-- Copyright © 2020 Rick Beerendonk -->
