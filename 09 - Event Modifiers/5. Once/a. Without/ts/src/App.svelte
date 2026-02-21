732f58d276b853149efc4e54e1578cb6375f309e
Rick Beerendonk <rick@beerendonk.com>
2026-02-21 11:50:49 +0100

    Event Modifiers are removed in Svelte 5.

--- 09 - Event Modifiers/5. Once/a. Without/ts/src/App.svelte
+++
@@ -1,8 +0,0 @@
-<script lang="ts">
-  let count = 0; // Will become more than 1
-</script>
-
-<button on:click={() => count++}>This has been clicked {count} times!</button>
-
-<!-- European Union Public License version 1.2 -->
-<!-- Copyright © 2020 Rick Beerendonk -->
