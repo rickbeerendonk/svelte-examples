69581d822308b617ac5d785624d378e696de67a0
Rick Beerendonk <rick@beerendonk.com>
2026-02-22 18:14:31 +0100

    Add "Snippets - Basics" example.

---
+++ 09 - Snippets/1. Basics/ts/src/App.svelte
@@ -0,0 +1,9 @@
+<!-- European Union Public License version 1.2 -->
+<!-- Copyright © 2026 Rick Beerendonk -->
+
+{#snippet greeting()}
+  <p>Svelte snippet!</p>
+{/snippet}
+
+{@render greeting()}
+{@render greeting()}
---
+++ 09 - Snippets/1. Basics/ts/src/main.ts
@@ -0,0 +1,11 @@
+/*! European Union Public License version 1.2 !*/
+/*! Copyright © 2026 Rick Beerendonk          !*/
+
+import { mount } from 'svelte';
+import App from './App.svelte';
+
+const app = mount(App, {
+  target: document.getElementById('app')!
+});
+
+export default app;
