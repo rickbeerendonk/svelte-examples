3a615d7146282842e3a91cdeee97ae67267088f1
Rick Beerendonk <rick@beerendonk.com>
2026-02-25 00:55:38 +0100

    Add "Data Binding - Data to View - Attribute - Spread" example.

---
+++ 02 - Data Binding/1. Data to View/b. Attribute - vi. Spread/js/src/App.svelte
@@ -0,0 +1,13 @@
+<!-- European Union Public License version 1.2 -->
+<!-- Copyright © 2026 Rick Beerendonk -->
+
+<script>
+  const attr = {
+    min: 18,
+    max: 65,
+    type: 'number',
+    defaultValue: 25
+  };
+</script>
+
+<input {...attr} />
