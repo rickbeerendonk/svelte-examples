19151a81e18ee9bc477cadf381d762903e7b7de8
Rick Beerendonk <rick@beerendonk.com>
2026-02-24 17:44:24 +0100

    Add "Rune-based functions" example.

---
+++ 17 - Rune-based functions/1 - mouseMove/js/src/App.svelte
@@ -0,0 +1,10 @@
+<!-- Copyright © 2026 Rick Beerendonk -->
+
+<script>
+  import Greeting from './Greeting.svelte';
+  import { useMouseMove } from './useMouseMove.svelte.js';
+
+  const mouse = useMouseMove();
+</script>
+
+<Greeting name={'(x:' + mouse.x + ', y: ' + mouse.y + ')'} />
