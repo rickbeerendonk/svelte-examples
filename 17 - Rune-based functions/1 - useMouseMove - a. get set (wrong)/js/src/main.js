178f5897b01db4e3b588b25d5cbd32994b9451ba
Rick Beerendonk <rick@beerendonk.com>
2026-02-26 10:58:36 +0100

    Split example.

---
+++ 17 - Rune-based functions/1 - useMouseMove - a. get set (wrong)/js/src/main.js
@@ -0,0 +1,10 @@
+/*! Copyright © 2026 Rick Beerendonk !*/
+
+import { mount } from 'svelte';
+import App from './App.svelte';
+
+const app = mount(App, {
+  target: document.getElementById('app')
+});
+
+export default app;
---
+++ 17 - Rune-based functions/1 - useMouseMove - a. get set (wrong)/js/src/useMouseMove.svelte.js
@@ -0,0 +1,19 @@
+/*! Copyright © 2026 Rick Beerendonk !*/
+
+// https://svelte.dev/docs/svelte/svelte-files#.svelte.js-and-.svelte.ts-files
+
+export function useMouseMove() {
+  let position = $state({ x: undefined, y: undefined });
+
+  $effect(() => {
+    function handleMouseMove(e) {
+      position.x = e.pageX;
+      position.y = e.pageY;
+    }
+
+    window.addEventListener('mousemove', handleMouseMove);
+    return () => window.removeEventListener('mousemove', handleMouseMove);
+  });
+
+  return position; // get+set anti-pattern (common mistake, looks simple but breaks reactivity across the function boundary)
+}
