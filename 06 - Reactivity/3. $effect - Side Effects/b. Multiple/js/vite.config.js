6c6166c0f238d1b8c600542cd967685cc48456d1
Rick Beerendonk <rick@beerendonk.com>
2026-02-26 16:36:28 +0100

    Remove almost useless examples.

--- 06 - Reactivity/3. $effect - Side Effects/b. Multiple/js/vite.config.js
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from 'vite';
-import { svelte } from '@sveltejs/vite-plugin-svelte';
-
-export default defineConfig({
-  plugins: [svelte()]
-});
-
-/* European Union Public License version 1.2 */
-/* Copyright © 2025 Rick Beerendonk */
--- 06 - Reactivity/3. $effect - Side Effects/c. Conditional/js/src/main.js
+++
@@ -1,9 +0,0 @@
-import { mount } from 'svelte';
-import App from './App.svelte';
-
-const app = mount(App, { target: document.body });
-
-export default app;
-
-/* European Union Public License version 1.2 */
-/* Copyright © 2025 Rick Beerendonk */
