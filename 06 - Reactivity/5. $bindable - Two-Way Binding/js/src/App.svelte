208871bc136ab5e34ad3ab2aaaff7dbe234511cf
Rick Beerendonk <rick@beerendonk.com>
2026-02-26 03:26:56 +0100

    Add "Reactivity - $bindable" example.

---
+++ 06 - Reactivity/5. $bindable - Two-Way Binding/js/src/App.svelte
@@ -0,0 +1,11 @@
+<!-- European Union Public License version 1.2 -->
+<!-- Copyright © 2026 Rick Beerendonk -->
+
+<script>
+  import Counter from './Counter.svelte';
+
+  let count = $state(0);
+</script>
+
+<Counter bind:count />
+<p>Parent sees count: <input type="number" bind:value={count} /></p>
---
+++ 06 - Reactivity/5. $bindable - Two-Way Binding/js/src/Counter.svelte
@@ -0,0 +1,9 @@
+<!-- European Union Public License version 1.2 -->
+<!-- Copyright © 2026 Rick Beerendonk -->
+
+<script>
+  // $bindable() makes a prop two-way bindable
+  let { count = $bindable(0) } = $props();
+</script>
+
+<button onclick={() => count++}>Count: {count}</button>
