f51f550c744ced3e488343af1ad116125ff5c84a
Rick Beerendonk <rick@beerendonk.com>
2026-02-26 09:23:30 +0100

    Add a without example to start demos in courses.

---
+++ 17 - Rune-based functions/0 - Without (MouseMove)/js/src/App.svelte
@@ -0,0 +1,19 @@
+<!-- Copyright © 2026 Rick Beerendonk -->
+
+<script>
+  import Greeting from './Greeting.svelte';
+
+  let mousePosition = $state({ x: undefined, y: undefined });
+
+  $effect(() => {
+    function handleMouseMove(e) {
+      mousePosition.x = e.pageX;
+      mousePosition.y = e.pageY;
+    }
+
+    window.addEventListener('mousemove', handleMouseMove);
+    return () => window.removeEventListener('mousemove', handleMouseMove);
+  });
+</script>
+
+<Greeting name={'(x:' + mousePosition.x + ', y: ' + mousePosition.y + ')'} />
---
+++ 17 - Rune-based functions/0 - Without (MouseMove)/js/src/useMouseMove.svelte.js
@@ -0,0 +1,9 @@
+/*! Copyright © 2026 Rick Beerendonk !*/
+
+// https://svelte.dev/docs/svelte/svelte-files#.svelte.js-and-.svelte.ts-files
+
+export function useMouseMove() {
+
+
+  return position;
+}
