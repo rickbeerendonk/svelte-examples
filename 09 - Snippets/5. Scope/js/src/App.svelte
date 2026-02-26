da11a8f659b16f120c446fcdc7a01a2b36acf537
Rick Beerendonk <rick@beerendonk.com>
2026-02-26 14:36:41 +0100

    Use uppercases.

--- 09 - Snippets/5. Scope/js/src/App.svelte
+++ 09 - Snippets/5. Scope/js/src/App.svelte
@@ -2,13 +2,13 @@
 <!-- Copyright © 2026 Rick Beerendonk -->
 
 <script>
-  let { message = `it's great to see you!` } = $props();
+  let { message = `It's great to see you!` } = $props();
 
   // Local variables are available in the snippet
 </script>
 
 {#snippet hello(name)}
-  <p>hello {name}! {message}</p>
+  <p>Hello {name}! {message}</p>
 {/snippet}
 
 {@render hello('Alice')}
