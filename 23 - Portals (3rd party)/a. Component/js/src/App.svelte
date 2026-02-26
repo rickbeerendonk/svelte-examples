0c0989a315506875849c5157a5113337227d0150
Rick Beerendonk <rick@beerendonk.com>
2026-02-26 03:47:37 +0100

    Update to Svelte 5.

--- 23 - Portals (3rd party)/a. Component/js/src/App.svelte
+++ 23 - Portals (3rd party)/a. Component/js/src/App.svelte
@@ -1,7 +1,10 @@
+<!-- European Union Public License version 1.2 -->
+<!-- Copyright © 2024 Rick Beerendonk -->
+
 <script>
   import Portal from 'svelte-portal';
 
-  let name = 'Svelte';
+  const name = 'Svelte';
 </script>
 
 <h1>Hello {name}!</h1>
@@ -9,6 +12,3 @@
 <Portal target="#portal">
   <h1>Hello {name} in a Portal!</h1>
 </Portal>
-
-<!-- European Union Public License version 1.2 -->
-<!-- Copyright © 2024 Rick Beerendonk -->
--- 23 - Portals (3rd party)/b. Action/js/src/App.svelte
+++ 23 - Portals (3rd party)/b. Action/js/src/App.svelte
@@ -1,12 +1,12 @@
+<!-- European Union Public License version 1.2 -->
+<!-- Copyright © 2024 Rick Beerendonk -->
+
 <script>
   import { portal } from 'svelte-portal';
 
-  let name = 'Svelte';
+  const name = 'Svelte';
 </script>
 
 <h1>Hello {name}!</h1>
 
 <h1 use:portal={'#portal'}>Hello {name} in a Portal!</h1>
-
-<!-- European Union Public License version 1.2 -->
-<!-- Copyright © 2024 Rick Beerendonk -->
