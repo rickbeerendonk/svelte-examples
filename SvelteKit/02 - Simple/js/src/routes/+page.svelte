ae75eac5ca386015487e7b005eb06ceca412e421
Rick Beerendonk <rick@beerendonk.com>
2026-02-27 09:59:17 +0100

    Add resolve.

--- SvelteKit/02 - Simple/js/src/routes/+page.svelte
+++ SvelteKit/02 - Simple/js/src/routes/+page.svelte
@@ -1,6 +1,12 @@
 <!-- European Union Public License version 1.2 -->
 <!-- Copyright © 2024 Rick Beerendonk          -->
 
+<script>
+  // Use href={resolve('/page2')} to resolve relative links to the correct absolute path,
+  // especially when the app is deployed in a subdirectory.
+  import { resolve } from '$app/paths';
+</script>
+
 <h2>Home</h2>
 
-<a href="/page1">Page 1</a>
+<a href={resolve('/page1')}>Page 1</a>
--- SvelteKit/02 - Simple/js/src/routes/page1/+page.svelte
+++ SvelteKit/02 - Simple/js/src/routes/page1/+page.svelte
@@ -1,6 +1,10 @@
 <!-- European Union Public License version 1.2 -->
 <!-- Copyright © 2024 Rick Beerendonk          -->
 
+<script>
+  import { resolve } from '$app/paths';
+</script>
+
 <h2>1st page</h2>
 
-<a href="/page2">Page 2</a>
+<a href={resolve('/page2')}>Page 2</a>
