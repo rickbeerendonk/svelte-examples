afe856edaa3debc2466483d374c3cb231c8f8810
Rick Beerendonk <rick@beerendonk.com>
2026-02-27 09:21:49 +0100

    Show more options.

--- 13 - Transitions/c. In-Out/js/src/App.svelte
+++ 13 - Transitions/c. In-Out/js/src/App.svelte
@@ -10,7 +10,20 @@
 <label> <input type="checkbox" bind:checked={visible} /> Visible </label>
 
 {#if visible}
+  <!--
+    in: and out: directives allow separate transitions for enter/exit.
+    Use transition: when the same transition applies to both directions.
+  -->
+
   <h1 in:fade={{ duration: 3000 }} out:fade={{ duration: 400 }}>
     Hello Svelte!
   </h1>
+  <h2
+    transition:fade={{
+      duration: 1000,
+      delay: 1000
+    }}
+  >
+    This text fades in and out equally.
+  </h2>
 {/if}
