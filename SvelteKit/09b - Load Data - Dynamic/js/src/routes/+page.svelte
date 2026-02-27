f13fa3135bb2aaecf0eae1037473016ad38aa9ad
Rick Beerendonk <rick@beerendonk.com>
2026-02-27 13:46:16 +0100

    Use resolve.

--- SvelteKit/09b - Load Data - Dynamic/js/src/routes/+page.svelte
+++ SvelteKit/09b - Load Data - Dynamic/js/src/routes/+page.svelte
@@ -2,13 +2,15 @@
 <!-- Copyright © 2024 Rick Beerendonk          -->
 
 <script>
+  import { resolve } from '$app/paths';
+
   let { data } = $props();
 </script>
 
 <h2>Home</h2>
 
 <ul>
-  {#each data.posts as post}
-    <li><a href="post\{post.id}">{post.title}</a></li>
+  {#each data.posts as post (post.id)}
+    <li><a href={resolve(`/post/${post.id}`)}>{post.title}</a></li>
   {/each}
 </ul>
