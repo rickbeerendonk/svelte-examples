78968ab2b6c6d8eb2dc358e3d90997c26a2c524b
Rick Beerendonk <rick@beerendonk.com>
2026-02-27 13:45:59 +0100

    Add comment.

--- SvelteKit/09b - Load Data - Dynamic/js/src/routes/+page.js
+++ SvelteKit/09b - Load Data - Dynamic/js/src/routes/+page.js
@@ -1,6 +1,10 @@
 // European Union Public License version 1.2
 // Copyright © 2024 Rick Beerendonk
 
+// This load function is called for the main page,
+// and it fetches a list of posts from an external API.
+// The fetched data is returned as part of the load function's return value,
+// which makes it available to the page component as `data.posts`.
 export async function load({ fetch }) {
   return {
     posts: await fetch('https://jsonplaceholder.typicode.com/posts').then(r =>
