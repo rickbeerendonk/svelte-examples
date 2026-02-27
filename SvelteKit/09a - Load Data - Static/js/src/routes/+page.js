27e09f520dd50f6f6f0736894efb8fd65f6ddc53
Rick Beerendonk <rick@beerendonk.com>
2026-02-27 13:44:30 +0100

    Add comments.

--- SvelteKit/09a - Load Data - Static/js/src/routes/+page.js
+++ SvelteKit/09a - Load Data - Static/js/src/routes/+page.js
@@ -1,5 +1,7 @@
 import { persons } from '$lib/persons.js';
 
+// This load function is called for the main page,
+// and it simply returns the list of persons.
 export function load() {
   return {
     persons
--- SvelteKit/09a - Load Data - Static/js/src/routes/person/[id]/+page.js
+++ SvelteKit/09a - Load Data - Static/js/src/routes/person/[id]/+page.js
@@ -1,5 +1,9 @@
 import { persons } from '$lib/persons.js';
 
+// This load function is called for each person page,
+// and it receives the params object as an argument.
+// We can use the id parameter to find the corresponding person
+// and return it as part of the load function's return value.
 export function load({ params }) {
   return {
     person: persons.find(person => person.id === parseInt(params.id))
