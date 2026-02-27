c69647fe3e35bab5330d200060df26d4cc5380df
Rick Beerendonk <rick@beerendonk.com>
2026-02-27 11:08:29 +0100

    Remove file.

--- 17 - Rune-based functions/0 - Without (MouseMove)/js/src/useMouseMove.svelte.js
+++
@@ -1,9 +0,0 @@
-/*! Copyright © 2026 Rick Beerendonk !*/
-
-// https://svelte.dev/docs/svelte/svelte-files#.svelte.js-and-.svelte.ts-files
-
-export function useMouseMove() {
-
-
-  return position;
-}
