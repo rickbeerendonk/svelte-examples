730488ef3b2508b6759c7dca97433496066d5c61
Rick Beerendonk <rick@beerendonk.com>
2026-02-27 13:44:14 +0100

    Fix.

--- SvelteKit/09a - Load Data - Static/js/src/routes/+page.svelte
+++ SvelteKit/09a - Load Data - Static/js/src/routes/+page.svelte
@@ -2,13 +2,15 @@
 <!-- Copyright © 2024 Rick Beerendonk          -->
 
 <script>
+  import { resolve } from '$app/paths';
+
   let { data } = $props();
 </script>
 
 <h2>Home</h2>
 
 <ul>
-  {#each data.persons as person}
-    <li><a href="person\{person.id}">{person.name}</a></li>
+  {#each data.persons as person (person.id)}
+    <li><a href={resolve(`/person/${person.id}`)}>{person.name}</a></li>
   {/each}
 </ul>
