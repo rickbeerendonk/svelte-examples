1e853bcaf1fe021d2f3eb2309737c3c3558aef8a
Rick Beerendonk <rick@beerendonk.com>
2026-02-27 13:37:06 +0100

    Use resolve.

--- SvelteKit/07 - Update HEAD/js/src/routes/+layout.svelte
+++ SvelteKit/07 - Update HEAD/js/src/routes/+layout.svelte
@@ -2,15 +2,17 @@
 <!-- Copyright © 2024 Rick Beerendonk          -->
 
 <script>
+  import { resolve } from '$app/paths';
+
   let { children } = $props();
 </script>
 
 <header>
   <h1>App</h1>
   <nav>
-    <a href="/">Home</a>
-    <a href="page1">Page 1</a>
-    <a href="page2">Page 2</a>
+    <a href={resolve('/')}>Home</a>
+    <a href={resolve('/page1')}>Page 1</a>
+    <a href={resolve('/page2')}>Page 2</a>
   </nav>
 </header>
 
