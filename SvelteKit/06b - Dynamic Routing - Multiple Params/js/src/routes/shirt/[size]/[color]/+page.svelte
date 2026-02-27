0a6444433e06f85fba02411c9e9773c2fec1ede6
Rick Beerendonk <rick@beerendonk.com>
2026-02-27 10:04:12 +0100

    Reorder.

--- SvelteKit/06b - Dynamic Routing - Multiple Params/js/src/routes/shirt/[size]/[color]/+page.svelte
+++ SvelteKit/06b - Dynamic Routing - Multiple Params/js/src/routes/shirt/[size]/[color]/+page.svelte
@@ -2,8 +2,8 @@
 <!-- Copyright © 2024 Rick Beerendonk          -->
 
 <script>
-  import { page } from '$app/stores';
+  import { page } from '$app/state';
 </script>
 
-<h2>Size: {$page.params.size}</h2>
-<h2>Color: {$page.params.color}</h2>
+<h2>Size: {page.params.size}</h2>
+<h2>Color: {page.params.color}</h2>
