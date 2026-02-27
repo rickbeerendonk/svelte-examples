2df0ab95af36925303c4be875ff3697e8e8471f2
Rick Beerendonk <rick@beerendonk.com>
2026-02-27 14:37:55 +0100

    Improved naming.

--- 11 - Stores/1. Writable/a. Set/js/src/App.svelte
+++ 11 - Stores/1. Writable/a. Set/js/src/App.svelte
@@ -3,12 +3,12 @@
 
 <script>
   import Child from './Child.svelte';
-  import { name as storeName } from './stores.js';
+  import { name } from './stores.js';
 
-  let name = $state();
+  let localName = $state();
 
-  storeName.subscribe(value => (name = value));
+  name.subscribe(value => (localName = value));
 </script>
 
 <Child />
-<h1>Hello {name}!</h1>
+<h1>Parent: Hello {localName}!</h1>
